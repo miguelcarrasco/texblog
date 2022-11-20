8652be7fa34ea0bd6df0be672bdf320fab6246c3
Miguel Angel Carrasco <tlacaelel.software@gmail.com>
2022-11-19 21:01:28 -0600

    Adding latex support via katex and eleventy filters

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,46 @@
+const katex = require("katex");
+
 module.exports = function (eleventyConfig) {
 
-  eleventyConfig.addPassthroughCopy("./src/css");
-  eleventyConfig.addWatchTarget("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addWatchTarget("./src/css");
 
-  return {
-    dir: {
-      input: "src",
-      output: "public",
-    },
-  };
+    eleventyConfig.addFilter("latexdisplay", (content) => {
+        return content.replace(/\$\$([\s\S]+?)\$\$/g, (_, equation) => {
+            equation = "\\displaystyle " + equation
+            const cleanEquation = equation
+                .replace(/&lt;/g, "<")
+                .replace(/&gt;/g, ">")
+                .replace(/&amp;/g, "&")
+                .replace(/\\\n/g, "\\\\");
+            // console.log(equation)
+
+            return "<span class=\"katex-display\">"+katex.renderToString(cleanEquation, {
+                throwOnError: false,
+                output: "html",
+            })+"</span>";
+        });
+    });
+
+    eleventyConfig.addFilter("latex", (content) => {
+        return content.replace(/\$([\s\S]+?)\$/g, (_, equation) => {
+            const cleanEquation = equation
+                .replace(/&lt;/g, "<")
+                .replace(/&gt;/g, ">")
+                .replace(/&amp;/g, "&")
+                .replace(/\\\n/g, "\\\\");
+
+            return katex.renderToString(cleanEquation, {
+                throwOnError: false,
+                output: "html",
+            });
+        });
+    });
+
+    return {
+        dir: {
+            input: "src",
+            output: "public",
+        },
+    };
 };
